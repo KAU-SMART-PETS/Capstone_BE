a87900ea27d8a6cbf6e734a52d91f5d4919ae9f2
jun23314 <dlwnsfml@naver.com>
2024-05-08 13:45:06 +0900

    fix: Gender, PetType ENUM 변경 - getValue 함수 삭제 - fromInt 함수에서 else문 삭제

--- src/main/java/com/kau/capstone/global/common/PetType.java
+++ src/main/java/com/kau/capstone/global/common/PetType.java
@@ -5,16 +5,18 @@ public enum PetType {
 
     private final Integer value;
 
-    PetType(int value) {this.value = value;}
-    public int getValue() {
-        return value;
+    PetType(int value) {
+        this.value = value;
     }
 
     // 정수를 ENUM으로 변환하는 메서드
     public static PetType fromInt(int value) {
-        if(value == 1) return CAT;
-        else if(value == 2) return DOG;
-        else
-            throw new IllegalArgumentException("유효하지 않은 종류입니다.");
+        if (value == 1) {
+            return CAT;
+        } else if (value == 2) {
+            return DOG;
+        }
+
+        throw new IllegalArgumentException("유효하지 않은 종류입니다.");
     }
 }
