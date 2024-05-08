2372ae1391c8cd1b1252f6976f3dfd54ed798210
jun23314 <dlwnsfml@naver.com>
2024-05-08 15:29:00 +0900

    fix: PetController 변경 - ResponseStatus 어노테이션 추가

--- src/main/java/com/kau/capstone/domain/pet/controller/PetRestController.java
+++ src/main/java/com/kau/capstone/domain/pet/controller/PetRestController.java
@@ -4,8 +4,10 @@ import com.kau.capstone.domain.pet.dto.request.PetRegistRequest;
 import com.kau.capstone.domain.pet.service.PetService;
 import com.kau.capstone.global.common.ResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +18,7 @@ public class PetRestController {
     private final PetService petService;
 
     @PostMapping("/regist")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseDTO<String> createPetRegist(PetRegistRequest petRegistRequest) {
         petService.registPet(petRegistRequest);
         return ResponseDTO.res("성공적으로 반려동물 정보를 저장하였습니다.");
