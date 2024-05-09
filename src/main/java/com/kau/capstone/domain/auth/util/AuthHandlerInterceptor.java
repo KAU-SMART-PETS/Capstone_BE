a5ad3d96bcb35df2a0d475738a1fdb1f188bc5db
70825 <hello70825@gmail.com>
2024-05-09 16:21:33 +0900

    feat: 세션 기반 로그인 기능 추가

---
+++ src/main/java/com/kau/capstone/domain/auth/util/AuthHandlerInterceptor.java
@@ -0,0 +1,25 @@
+package com.kau.capstone.domain.auth.util;
+
+import com.kau.capstone.domain.auth.exception.AuthException;
+import com.kau.capstone.global.exception.ErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Objects;
+
+public class AuthHandlerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        HttpSession session = request.getSession(false);
+
+        if (Objects.isNull(session)) {
+            throw new AuthException(ErrorCode.LOGIN_MEMBER_NOT_FOUND);
+        }
+
+        return true;
+    }
+}
