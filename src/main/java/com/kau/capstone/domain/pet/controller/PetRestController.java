813a100106d4e2f3721cf068f47412b95b46d8b7
jun23314 <dlwnsfml@naver.com>
2024-05-14 16:31:00 +0900

    fix: dto 형식 변경에 따른 controller 변경

--- src/main/java/com/kau/capstone/domain/pet/controller/PetRestController.java
+++ src/main/java/com/kau/capstone/domain/pet/controller/PetRestController.java
@@ -2,13 +2,11 @@ package com.kau.capstone.domain.pet.controller;
 
 import com.kau.capstone.domain.pet.dto.request.PetRegistRequest;
 import com.kau.capstone.domain.pet.service.PetService;
-import com.kau.capstone.global.common.ResponseDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +17,7 @@ public class PetRestController {
     private final PetService petService;
 
     @PostMapping("/regist")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> createPetRegist(PetRegistRequest petRegistRequest) {
+    public ResponseEntity<String> createPetRegist(@RequestBody PetRegistRequest petRegistRequest) {
         petService.registPet(petRegistRequest);
         return ResponseEntity.ok().build();
     }
