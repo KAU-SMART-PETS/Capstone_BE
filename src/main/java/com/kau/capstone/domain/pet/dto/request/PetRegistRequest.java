ba8264edff76497de4603e88204b226f7c2be936
jun23314 <dlwnsfml@naver.com>
2024-05-14 16:30:07 +0900

    fix: pet 등록 dto 형식 변경 - record -> class

--- src/main/java/com/kau/capstone/domain/pet/dto/request/PetRegistRequest.java
+++ src/main/java/com/kau/capstone/domain/pet/dto/request/PetRegistRequest.java
@@ -1,28 +1,30 @@
 package com.kau.capstone.domain.pet.dto.request;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record PetRegistRequest(
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PetRegistRequest {
+
     @NotEmpty(message = "이름을 입력해주세요")
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{1,10}$", message = "이름은 특수문자를 제외한 1~10자리여야 합니다.")
-    String name,
+    private String name;
 
-    int petType,
+    private Integer petType;
 
-    int gender,
+    private Integer gender;
 
-    float weight,
+    @Nullable
+    private float weight;
 
-    String imageUrl,
+    private String imageUrl;
 
-    Integer age
-
-) {
-
-    @Builder
-    public PetRegistRequest {
-    }
-
+    @NotNull
+    private Integer age;
 }
