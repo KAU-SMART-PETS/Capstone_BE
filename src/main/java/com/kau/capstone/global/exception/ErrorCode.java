b078c08d4d52337697a4a90c5db61a119882c8e7
jun23314 <dlwnsfml@naver.com>
2024-05-28 14:00:25 +0900

    feat: 반려동물 정보 조회 api 생성

--- src/main/java/com/kau/capstone/global/exception/ErrorCode.java
+++ src/main/java/com/kau/capstone/global/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     ALREADY_EXIST_ID_EXCEPTION(HttpStatus.BAD_REQUEST, "이미 사용중인 아이디입니다."),
     NOT_EXIST_PET_TYPE(HttpStatus.BAD_REQUEST, "존재하지 않는 반려동물 종류입니다."),
     INVALID_GENDER(HttpStatus.BAD_REQUEST, "유효하지 않은 성별입니다."),
-    LOGIN_MEMBER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "로그인 되지 않았거나, 존재하지 않는 유저로 로그인중입니다.")
+    LOGIN_MEMBER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "로그인 되지 않았거나, 존재하지 않는 유저로 로그인중입니다."),
+    PET_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "반려동물 정보를 찾을 수 없습니다.")
     ;//Error Code를 작성한 마지막에 ;를 추가합니다.
 
     private final HttpStatus httpStatus;
