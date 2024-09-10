fc75bc6603e152fa3224781860477bf4e61ce955
Dabeen Jeong <hello70825@gmail.com>
2024-09-11 02:00:18 +0900

    feat: 반려동물 정보 조회 api 생성 (#34)
    Co-authored-by: jun23314 <dlwnsfml@naver.com>

---
+++ src/main/java/com/kau/capstone/domain/pet/dto/response/PetInfoResponse.java
@@ -0,0 +1,28 @@
+package com.kau.capstone.domain.pet.dto.response;
+
+import com.kau.capstone.global.common.Gender;
+import com.kau.capstone.global.common.PetType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PetInfoResponse {
+
+    private String name;
+
+    private PetType petType;
+
+    private Gender gender;
+
+    private float weight;
+
+    private String imageUrl;
+
+    private Integer age;
+}
---
+++ src/main/java/com/kau/capstone/domain/pet/exception/PetNotFoundException.java
@@ -0,0 +1,11 @@
+package com.kau.capstone.domain.pet.exception;
+
+import com.kau.capstone.global.exception.ApplicationException;
+import com.kau.capstone.global.exception.ErrorCode;
+
+public class PetNotFoundException extends ApplicationException {
+
+    public PetNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
