52b7ebad943b459644ca994318e9247a6569a679
70825 <hello70825@gmail.com>
2024-10-12 20:16:27 +0900

    refactor: 동물병원 세부 정보 조회 메서드 네이밍 변경

--- src/main/java/com/kau/capstone/domain/vet/controller/VetController.java
+++ src/main/java/com/kau/capstone/domain/vet/controller/VetController.java
@@ -6,7 +6,6 @@ import com.kau.capstone.domain.vet.dto.VetsResponse;
 import com.kau.capstone.domain.vet.service.VetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,13 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class VetController implements VetApi{
+public class VetController implements VetApi {
 
     private final VetService vetService;
 
     @GetMapping(path = "/api/v1/vets/{vetId}")
-    public ResponseEntity<VetDetailResponse> getVetInfo(@PathVariable Long vetId,
-                                                        @RequestBody MemberLocationRequest memberLocationRequest) {
+    public ResponseEntity<VetDetailResponse> getVetDetailInfo(@PathVariable Long vetId,
+                                                              @RequestBody MemberLocationRequest memberLocationRequest) {
         VetDetailResponse response = vetService.getVetInfo(vetId, memberLocationRequest);
 
         return ResponseEntity.ok(response);
