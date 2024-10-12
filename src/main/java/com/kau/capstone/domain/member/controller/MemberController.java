5ac70da08e796b1cad48a329f2752985bf7f757f
70825 <hello70825@gmail.com>
2024-10-12 20:01:46 +0900

    refactor: controller -> restcontroller 변경

--- src/main/java/com/kau/capstone/domain/member/controller/MemberController.java
+++ src/main/java/com/kau/capstone/domain/member/controller/MemberController.java
@@ -12,8 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
--- src/main/java/com/kau/capstone/domain/vet/controller/VetController.java
+++ src/main/java/com/kau/capstone/domain/vet/controller/VetController.java
@@ -10,10 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-public class VetController {
+public class VetController implements VetApi{
 
     private final VetService vetService;
 
