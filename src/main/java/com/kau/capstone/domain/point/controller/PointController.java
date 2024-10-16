e17e3e43dded50b4411ce3487a1a2165ca117338
70825 <hello70825@gmail.com>
2024-10-16 20:42:05 +0900

    feat: 포인트 API Swagger 추가

--- src/main/java/com/kau/capstone/domain/point/controller/PointController.java
+++ src/main/java/com/kau/capstone/domain/point/controller/PointController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class PointController {
+public class PointController implements PointApi {
 
     private final PointService pointService;
 
