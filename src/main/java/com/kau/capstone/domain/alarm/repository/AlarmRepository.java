c0be046abbe5e4a2e1c617d42141f8838ca2d269
70825 <hello70825@gmail.com>
2024-10-16 16:38:41 +0900

    feat: 알람 기능 추가

---
+++ src/main/java/com/kau/capstone/domain/alarm/repository/AlarmRepository.java
@@ -0,0 +1,15 @@
+package com.kau.capstone.domain.alarm.repository;
+
+import com.kau.capstone.domain.alarm.entity.Alarm;
+import com.kau.capstone.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+
+    @Query("SELECT a FROM Alarm a WHERE a.member = :member AND a.isVisible = true")
+    List<Alarm> findAllAlarmByMember(@Param("member") Member member);
+}
