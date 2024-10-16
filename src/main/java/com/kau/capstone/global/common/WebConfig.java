708a34b651d20a1a750fdb70e6042d06bf9e5163
70825 <hello70825@gmail.com>
2024-10-16 18:49:57 +0900

    feat: 인터셉터 적용

--- src/main/java/com/kau/capstone/global/common/WebConfig.java
+++ src/main/java/com/kau/capstone/global/common/WebConfig.java
@@ -2,6 +2,7 @@ package com.kau.capstone.global.common;
 
 import com.kau.capstone.domain.auth.util.AuthArgumentResolver;
 import com.kau.capstone.domain.auth.util.AuthHandlerInterceptor;
+import com.kau.capstone.domain.pet.util.PetHandlerInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -17,6 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthArgumentResolver authArgumentResolver;
     private final AuthHandlerInterceptor authHandlerInterceptor;
+    private final PetHandlerInterceptor petHandlerInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -26,9 +28,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authHandlerInterceptor)
-                .addPathPatterns("/api/v1/users", "/api/members/**")
-                .addPathPatterns("/api/v1/pets")
-                .addPathPatterns("/api/v1/logout");
+                .addPathPatterns("/api/v1/users", "/api/v1/users/**")
+                .addPathPatterns("/api/v1/logout")
+                .addPathPatterns("/api/v1/points", "/api/v1/points/**")
+                .addPathPatterns("/api/v1/foods/**").excludePathPatterns("/api/v1/foods")
+                .addPathPatterns("/api/v1/bluetooth")
+                .addPathPatterns("/api/v1/rewards", "/api/v1/rewards/**")
+                .addPathPatterns("/api/v1/alarm")
+                .addPathPatterns("/api/v1/logout")
+                .addPathPatterns("/api/v1/oauth2/logout");
+        registry.addInterceptor(petHandlerInterceptor)
+                .addPathPatterns("/api/v1/pets", "/api/v1/pets/**");
     }
 
     @Override
