28f93a41a4c0a8b0eeac1530ee08b695ded27e40
70825 <hello70825@gmail.com>
2024-10-17 14:42:32 +0900

    fix: 보건정보 조회를 예방접종 최신순으로 변경

--- src/main/java/com/kau/capstone/domain/vaccination/repository/VaccinationRepository.java
+++ src/main/java/com/kau/capstone/domain/vaccination/repository/VaccinationRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 
 public interface VaccinationRepository extends JpaRepository<Vaccination, Long> {
 
-    @Query("SELECT v FROM Vaccination v WHERE v.pet = :pet")
+    @Query("SELECT v FROM Vaccination v WHERE v.pet = :pet ORDER BY v.timeYear DESC, v.timeMonth DESC, v.timeDay DESC")
     List<Vaccination> findAllByMemberAndPet(@Param("pet") Pet pet);
 }
