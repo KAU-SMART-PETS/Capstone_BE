bb604f49ed85201d10372b45875be6466d5a6f45
Dabeen Jeong <hello70825@gmail.com>
2024-10-17 12:43:52 +0900

    feat: 예상하지 못한 에러 발생시 문제 원인을 파악할 수 있도록 메시지 추가 (#148)

--- src/main/java/com/kau/capstone/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kau/capstone/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.kau.capstone.global.exception;
 
 import com.kau.capstone.global.common.ResponseDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,5 +17,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
             .body(ResponseDTO.res(e.getMessage()));
 
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDTO<String>> exception(Exception e) {
+        log.error("Exception: ", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ResponseDTO.res(e.getMessage()));
     }
 }
