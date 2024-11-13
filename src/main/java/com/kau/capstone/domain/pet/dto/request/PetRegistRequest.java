1b52715eeb37992fe0b735ec776cc6761abaa4d3
jun23314 <116951160+jun23314@users.noreply.github.com>
2024-11-13 16:11:27 +0900

    fix: pet name 등록 시 대문자 영문도 허용 (#182)

--- src/main/java/com/kau/capstone/domain/pet/dto/request/PetRegistRequest.java
+++ src/main/java/com/kau/capstone/domain/pet/dto/request/PetRegistRequest.java
@@ -16,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class PetRegistRequest {
 
     @NotEmpty(message = "이름을 입력해주세요")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{1,10}$", message = "이름은 특수문자를 제외한 1~10자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{1,10}$", message = "이름은 특수문자를 제외한 1~10자리여야 합니다.")
     private String name;
 
     private Integer petType;
