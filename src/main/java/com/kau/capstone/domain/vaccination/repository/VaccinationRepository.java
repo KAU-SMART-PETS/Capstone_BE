6755d2097f3b889e4a64c780c061963a416edd1f
parkhs21 <2001phs92@gmail.com>
2025-03-05 10:48:17 +0900

    refactor: 불필요 native query 제거

--- src/main/java/com/kau/capstone/domain/vaccination/repository/VaccinationRepository.java
+++ src/main/java/com/kau/capstone/domain/vaccination/repository/VaccinationRepository.java
@@ -3,13 +3,11 @@ package com.kau.capstone.domain.vaccination.repository;
 import com.kau.capstone.domain.pet.entity.Pet;
 import com.kau.capstone.domain.vaccination.entity.Vaccination;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface VaccinationRepository extends JpaRepository<Vaccination, Long> {
 
-    @Query("SELECT v FROM Vaccination v WHERE v.pet = :pet ORDER BY v.vaccinatedAt DESC")
-    List<Vaccination> findAllByPet(@Param("pet") Pet pet);
+    List<Vaccination> findAllByPetOrderByVaccinatedAtDesc(@Param("pet") Pet pet);
 }
