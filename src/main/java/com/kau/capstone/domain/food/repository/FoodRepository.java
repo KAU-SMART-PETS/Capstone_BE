1d10d594253f0dd6c78ec3d8866d86fe68e463d2
한승민 <ehrehrdltkfk@gmail.com>
2025-03-07 23:39:00 +0900

    feat : getById 생성

--- src/main/java/com/kau/capstone/domain/food/repository/FoodRepository.java
+++ src/main/java/com/kau/capstone/domain/food/repository/FoodRepository.java
@@ -1,7 +1,11 @@
 package com.kau.capstone.domain.food.repository;
 
 import com.kau.capstone.domain.food.entity.Food;
+import com.kau.capstone.domain.food.exception.FoodNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
+    default Food getById(long id){
+        return findById(id).orElseThrow(FoodNotFoundException::new);
+    }
 }
