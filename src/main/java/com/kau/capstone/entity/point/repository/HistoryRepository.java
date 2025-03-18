d6f77cd7eb919a21206bf190cd02d58def022b98
한승민 <ehrehrdltkfk@gmail.com>
2025-03-18 16:05:00 +0900

    feat: findHistoryListByMember 메서드 생성

--- src/main/java/com/kau/capstone/entity/point/repository/HistoryRepository.java
+++ src/main/java/com/kau/capstone/entity/point/repository/HistoryRepository.java
@@ -2,6 +2,7 @@ package com.kau.capstone.entity.point.repository;
 
 import com.kau.capstone.entity.member.Member;
 import com.kau.capstone.entity.point.History;
+import com.kau.capstone.v2.point.dto.response.HistoryListResV2;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,11 @@ import java.util.List;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
-    @Query("SELECT h FROM History h WHERE h.member = :member ORDER BY h.id DESC ")
+    @Query("SELECT h FROM History h WHERE h.point.member = :member ORDER BY h.id DESC ")
     List<History> findHistoriesByMember(@Param("member") Member member);
+
+    default HistoryListResV2 findHistoryListByMember(Member member){
+        List<History> res = findHistoriesByMember(member);
+        return HistoryListResV2.toResponse(res);
+    }
 }
