bbdbea8c2d2a13b2eda9ba659f219110ad8a06f4
한승민 <ehrehrdltkfk@gmail.com>
2025-03-18 16:05:22 +0900

    feat: PointServiceV2 생성

---
+++ src/main/java/com/kau/capstone/v2/point/service/PointServiceV2.java
@@ -0,0 +1,59 @@
+package com.kau.capstone.v2.point.service;
+
+import com.kau.capstone.entity.member.Member;
+import com.kau.capstone.entity.member.repository.MemberRepository;
+import com.kau.capstone.entity.point.History;
+import com.kau.capstone.entity.point.Point;
+import com.kau.capstone.entity.point.PointType;
+import com.kau.capstone.entity.point.repository.HistoryRepository;
+import com.kau.capstone.entity.point.repository.PointRepository;
+import com.kau.capstone.v2.point.dto.request.DepositPointReqV2;
+import com.kau.capstone.v2.point.dto.request.PayPointReqV2;
+import com.kau.capstone.v2.point.dto.response.HistoryListResV2;
+import com.kau.capstone.v2.point.exception.PointNotEnoughExceptionV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PointServiceV2 {
+
+    private final MemberRepository memberRepository;
+    private final HistoryRepository historyRepository;
+    private final PointRepository pointRepository;
+
+    public void payPoint(Long memberId, PayPointReqV2 request) {
+        Member member = memberRepository.getMemberById(memberId);
+        Point point = pointRepository.getByMember(member);
+
+        checkAmount(point.getAmount(), request.point());
+
+        point.payment(request.point());
+
+        History history = History.of(point, request, PointType.PAYMENT_POINT);
+        historyRepository.save(history);
+    }
+
+    public void depositPoint(Long memberId, DepositPointReqV2 request) {
+        Member member = memberRepository.getMemberById(memberId);
+        Point point = pointRepository.getByMember(member);
+
+        point.deposit(request.point());
+
+        History history = History.of(point, request, PointType.PAYMENT_POINT);
+        historyRepository.save(history);
+    }
+
+    public HistoryListResV2 getPointHistory(Long memberId) {
+        Member member = memberRepository.getMemberById(memberId);
+        return historyRepository.findHistoryListByMember(member);
+    }
+
+    private void checkAmount(long amount, long point){
+        if (amount < point) {
+            throw new PointNotEnoughExceptionV2();
+        }
+    }
+}
