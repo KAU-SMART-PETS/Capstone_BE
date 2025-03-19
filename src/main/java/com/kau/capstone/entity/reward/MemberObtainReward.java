8e341b54abf3a228e76e95867a8311e13edd39e8
jun23314 <116951160+jun23314@users.noreply.github.com>
2025-03-19 19:44:51 +0900

    refactor: 리워드 엔티티 리팩토링 (#224)

---
+++ src/main/java/com/kau/capstone/entity/reward/MemberObtainReward.java
@@ -0,0 +1,56 @@
+package com.kau.capstone.entity.reward;
+
+import com.kau.capstone.entity.member.Member;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.io.Serializable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.joda.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberObtainReward {
+
+    @EmbeddedId
+    private MemberRewardId id;
+
+    @Comment("리워드 달성 일자")
+    private LocalDate achievedAt;
+
+    @Comment("리워드 획득 일자")
+    private LocalDate obtainedAt;
+
+    private MemberObtainReward(Member member, Reward reward,
+        LocalDate achievedAt, LocalDate obtainedAt) {
+        this.id = new MemberRewardId(member, reward);
+        this.achievedAt = achievedAt;
+        this.obtainedAt = obtainedAt;
+    }
+
+    @Embeddable
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public class MemberRewardId implements Serializable {
+
+        @ManyToOne
+        @JoinColumn(name = "member_id")
+        private Member member;
+        @ManyToOne
+        @JoinColumn(name = "reward_id")
+        private Reward reward;
+
+        private MemberRewardId(Member member, Reward reward) {
+            this.member = member;
+            this.reward = reward;
+        }
+
+    }
+
+
+}
--- src/main/java/com/kau/capstone/entity/reward/repository/RewardRepository.java
+++ src/main/java/com/kau/capstone/entity/reward/repository/RewardRepository.java
@@ -1,19 +1,11 @@
 package com.kau.capstone.entity.reward.repository;
 
-import com.kau.capstone.entity.member.Member;
 import com.kau.capstone.entity.reward.Reward;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface RewardRepository extends JpaRepository<Reward, Long> {
 
-    @Query("SELECT r FROM Reward r WHERE r.member = :member ORDER BY r.type ASC")
-    List<Reward> findRewardsByMember(@Param("member") Member member);
-
-    @Query("SELECT r FROM Reward r WHERE r.member = :member AND r.type = :type")
-    Reward findRewardByMemberAndType(@Param("member") Member member,
-                                     @Param("type") Long type);
+    List<Reward> findRewards();
 }
