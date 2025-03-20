54736b2d311b9980bb08727a9b72613aa023f6c4
jun23314 <dlwnsfml@naver.com>
2025-03-20 15:24:57 +0900

    fix: 리워드 조회 시 생성/수정/삭제 일자 추가

--- src/main/java/com/kau/capstone/v2/reward/dto/RewardResV2.java
+++ src/main/java/com/kau/capstone/v2/reward/dto/RewardResV2.java
@@ -1,12 +1,16 @@
 package com.kau.capstone.v2.reward.dto;
 
 import com.kau.capstone.entity.reward.Reward;
+import java.time.LocalDateTime;
 
 public record RewardResV2(
     long id,
     String title,
     String content,
-    int earnPoint
+    int earnPoint,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt,
+    LocalDateTime deletedAt
 ) {
 
     public static RewardResV2 of(Reward reward) {
@@ -14,7 +18,10 @@ public record RewardResV2(
             reward.getId(),
             reward.getTitle(),
             reward.getContent(),
-            reward.getEarnPoint()
+            reward.getEarnPoint(),
+            reward.getCreatedAt(),
+            reward.getUpdatedAt(),
+            reward.getDeletedAt()
         );
     }
 
