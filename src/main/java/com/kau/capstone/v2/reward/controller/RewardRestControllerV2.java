ddfe97b68546fe160babec8b8c8dc5c1d1bbdbe4
jun23314 <dlwnsfml@naver.com>
2025-03-20 15:21:41 +0900

    feat: 리워드 삭제 API 작성

--- src/main/java/com/kau/capstone/v2/reward/controller/RewardRestControllerV2.java
+++ src/main/java/com/kau/capstone/v2/reward/controller/RewardRestControllerV2.java
@@ -12,6 +12,7 @@ import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,4 +68,12 @@ public class RewardRestControllerV2 {
         return ApiResponse.ok();
     }
 
+    @DeleteMapping("/{rewardId}")
+    public ResponseEntity<ApiResponse<String>> deleteReward(
+        @LoginUser LoginInfo loginInfo,
+        @PathVariable long rewardId
+    ) {
+        rewardService.deleteReward(loginInfo, rewardId);
+        return ApiResponse.ok();
+    }
 }
