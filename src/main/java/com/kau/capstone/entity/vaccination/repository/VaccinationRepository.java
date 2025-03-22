ad1f8ec1dc12a7492904be0b203e4efc0a2f4e08
parkhs21 <2001phs92@gmail.com>
2025-03-23 01:15:40 +0900

    refactor: @Query 삭제에 따른, @Param 삭제

--- src/main/java/com/kau/capstone/entity/vaccination/repository/VaccinationRepository.java
+++ src/main/java/com/kau/capstone/entity/vaccination/repository/VaccinationRepository.java
@@ -5,13 +5,12 @@ import com.kau.capstone.entity.vaccination.Vaccination;
 import com.kau.capstone.v2.vaccination.exception.VaccinationNotFoundExceptionV2;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface VaccinationRepository extends JpaRepository<Vaccination, Long> {
 
-    List<Vaccination> findAllByPetOrderByVaccinatedAtDesc(@Param("pet") Pet pet);
+    List<Vaccination> findAllByPetOrderByVaccinatedAtDesc(Pet pet);
 
     default @NonNull Vaccination getById(@NonNull Long id) {
         return findById(id).orElseThrow(VaccinationNotFoundExceptionV2::new);
