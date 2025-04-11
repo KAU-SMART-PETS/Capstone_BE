339bfe03c668b0300f47307fa064efb6006017ee
jun23314 <dlwnsfml@naver.com>
2025-04-11 17:06:34 +0900

    fix: 강아지 고양이 반환값 분리

--- src/main/java/com/kau/capstone/v2/ai/service/AIServiceV2.java
+++ src/main/java/com/kau/capstone/v2/ai/service/AIServiceV2.java
@@ -1,8 +1,10 @@
 package com.kau.capstone.v2.ai.service;
 
 
-import com.kau.capstone.entity.AI.Eyes;
-import com.kau.capstone.entity.AI.Repository.EyesRepository;
+import com.kau.capstone.entity.AI.CatEyes;
+import com.kau.capstone.entity.AI.DogEyes;
+import com.kau.capstone.entity.AI.Repository.CatEyesRepository;
+import com.kau.capstone.entity.AI.Repository.DogEyesRepository;
 import com.kau.capstone.entity.member.Member;
 import com.kau.capstone.entity.member.repository.MemberRepository;
 import com.kau.capstone.entity.pet.Pet;
@@ -10,7 +12,8 @@ import com.kau.capstone.entity.pet.repository.PetRepository;
 import com.kau.capstone.global.aiModel.AIModelClient;
 import com.kau.capstone.global.common.s3.FileService;
 import com.kau.capstone.v1.auth.dto.LoginInfo;
-import com.kau.capstone.v2.ai.dto.request.EyeReqV2;
+import com.kau.capstone.v2.ai.dto.response.CatEyeRes;
+import com.kau.capstone.v2.ai.dto.response.DogEyeRes;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,17 +25,15 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class AIServiceV2 {
 
-    private static final String[] diseases = {"conjunctivitis", "ulcerative_keratitis", "cataract",
-        "non_ulcerative_keratitis", "pigmentary_keratitis", "entropion", "blepharitis",
-        "eyelid_tumor", "incontinence", "nuclear_sclerosis", "corneal_dystrophy", "corneal_ulcer"};
 
+    private final CatEyesRepository catEyesRepository;
+    private final DogEyesRepository dogEyesRepository;
     private final MemberRepository memberRepository;
     private final PetRepository petRepository;
-    private final EyesRepository eyesRepository;
     private final AIModelClient aiModelClient;
     private final FileService fileService;
 
-    public EyeReqV2 analyzeEye(LoginInfo loginInfo, Long petId, MultipartFile imageFile) {
+    public Object analyzeEye(LoginInfo loginInfo, Long petId, MultipartFile imageFile) {
         Member member = memberRepository.getById(loginInfo.memberId());
         Pet pet = petRepository.getByIdAndDeletedAtIsNullAndMember(petId, member);
 
@@ -41,25 +42,15 @@ public class AIServiceV2 {
             pet.getPetType().toString());
         fileService.deleteImage(imageUrl);
 
-        Float[] prob = new Float[diseases.length];
-        for (int i = 0; i < diseases.length; ++i) {
-            if (aiResponse.containsKey(diseases[i])) {
-                prob[i] = (Float) aiResponse.get(diseases[i]);
-            } else {
-                prob[i] = null;
-            }
+        if (aiResponse.size() == 5) {
+            CatEyeRes catEyeRes = CatEyeRes.of(aiResponse);
+            catEyesRepository.save(CatEyes.of(catEyeRes, pet));
+            return catEyeRes;
         }
 
-        int idx = 0;
-        EyeReqV2 eyeRes = new EyeReqV2(
-            prob[idx++], prob[idx++], prob[idx++], prob[idx++], prob[idx++], prob[idx++],
-            prob[idx++], prob[idx++], prob[idx++], prob[idx++], prob[idx++], prob[idx]
-        );
-
-        Eyes eyes = Eyes.of(eyeRes, pet);
-        eyesRepository.save(eyes);
-
-        return eyeRes;
+        DogEyeRes dogEyeRes = DogEyeRes.of(aiResponse);
+        dogEyesRepository.save(DogEyes.of(dogEyeRes, pet));
+        return dogEyeRes;
     }
 
 }
